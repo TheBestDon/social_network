ebaec00518aefb43eb80f1307c71f16e24737030
TheBestDon <donatas_bakanas@yahoo.com>
2018-08-09 00:07:44 +0300

    user register route and password hash

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,7 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-// @route  GET api/posts/test
-// @desc   Tests post route
-// @access Public
-router.get('/test', (req, res) => res.json({msg: "Posts Works"}));
+// @route  GET api/posts/test @desc   Tests post route @access Public
+router.get('/test', (req, res) => res.json({msg: 'Posts Works'}))
 
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
-const users = require('./routes/api/users');
-const profile = require('./routes/api/profile');
-const posts = require('./routes/api/posts');
+const users = require('./routes/api/users')
+const profile = require('./routes/api/profile')
+const posts = require('./routes/api/posts')
 
-const app = express();
+const app = express()
 
-//DB Config
-const db = require('./config/keys').mongoURI;
+// Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
-//Connect to MongoDB
+// DB Config
+const db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
 mongoose
   .connect(db, {useNewUrlParser: true})
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.log(err))
 
-  app.get('/', (req, res) => res.send('Hello'));
+  app.get('/', (req, res) => res.send('Hello'))
 
-  //Use Routes
-  app.use('/api/users', users);
-  app.use('/api/profile', profile);
-  app.use('/api/posts', posts);
+// Use Routes
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`))
