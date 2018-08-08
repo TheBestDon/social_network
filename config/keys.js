a5f29c69a7b1c36a5cd6ecc61b44841c25494c55
TheBestDon <donatas_bakanas@yahoo.com>
2018-08-09 00:43:04 +0300

    jwt token returned

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://Donatas1:Zalgiris1@ds217092.mlab.com:17092/devconnector'
+  mongoURI: 'mongodb://Donatas1:Zalgiris1@ds217092.mlab.com:17092/devconnector',
+  secretOrKey: 'egwyhbwehqbweid'
 }
